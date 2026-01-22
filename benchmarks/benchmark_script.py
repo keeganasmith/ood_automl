2cdc4739127e4f41358e2feed7c87fc66c202335
Keegan <99233684+keeganasmith@users.noreply.github.com>
2026-01-22 12:03:42 -0600

    Add benchmark script for training time

---
+++ benchmarks/benchmark_script.py
@@ -0,0 +1,19 @@
+import time
+
+import pandas as pd
+from autogluon.tabular import TabularPredictor
+
+
+def main() -> None:
+    train_data = pd.read_csv("./sample_datasets/train.csv")
+
+    start_time = time.perf_counter()
+    TabularPredictor(label="Survived").fit(train_data)
+    end_time = time.perf_counter()
+
+    elapsed = end_time - start_time
+    print(f"SCRIPT_TRAIN_TIME_SECONDS={elapsed}")
+
+
+if __name__ == "__main__":
+    main()
